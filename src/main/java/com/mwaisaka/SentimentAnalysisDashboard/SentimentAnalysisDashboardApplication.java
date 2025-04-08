459c386bbb6a3cdfb6f86421d3dacb65aa2915de
Phenny <mwaisakaphenny@gmail.com>
2025-04-08 06:37:22 +0300

    Initial commit

---
+++ src/main/java/com/mwaisaka/SentimentAnalysisDashboard/SentimentAnalysisDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.mwaisaka.SentimentAnalysisDashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentimentAnalysisDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentimentAnalysisDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mwaisaka/SentimentAnalysisDashboard/SentimentAnalysisDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mwaisaka.SentimentAnalysisDashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentimentAnalysisDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
