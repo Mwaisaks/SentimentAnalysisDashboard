e3ef6a2b5d84f067910e606d71fcd60e1fdcecbe
Phenny <mwaisakaphenny@gmail.com>
2025-04-09 21:49:36 +0300

    Adding ML integrayion for single text

--- src/main/java/com/mwaisaka/SentimentAnalysisDashboard/service/SentimentServiceImpl.java
+++ src/main/java/com/mwaisaka/SentimentAnalysisDashboard/service/SentimentServiceImpl.java
@@ -2,15 +2,20 @@ package com.mwaisaka.SentimentAnalysisDashboard.service;
 
 import com.mwaisaka.SentimentAnalysisDashboard.dto.SentimentRequest;
 import com.mwaisaka.SentimentAnalysisDashboard.dto.SentimentResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SentimentServiceImpl implements SentimentService{
 
+    @Value("${ml.service.url}")
+    private String mlServiceUrl;
+
     @Override
     public SentimentResponse analyzeSentiment(SentimentRequest request) {
 
         //TODO: call ML model here
+
 
         //Mock response for now(replace with real ML integration)
         return new SentimentResponse("POSITIVE", 0.92);
